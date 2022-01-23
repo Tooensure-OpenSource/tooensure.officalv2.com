700ff0c4db8b52a3941df1aafb8e2b5eb4336655
Shawn Bellazan <tooensuremaster@gmail.com>
2022-01-23 00:21:11 -0600

    new build action

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class',
+    content: [
+    "./TooensureApp/wwwroot/**/*.{html,js,css}",
+    "./TooensureApp/**/**/*.razor",
+    "./TooensureApp/Pages/*.razor",
+    "./TooensureApp/Shared/**/*.razor"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
